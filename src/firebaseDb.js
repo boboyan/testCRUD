50b0b646f84d5f0cb922c114a87de0ebc2bac758
boboyan <byan@unomaha.edu>
2022-10-18 16:29:40 -0500

    2

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
-}
-
+// eslint-disable-next-line no-unused-vars
+const app = firebase.initializeApp ({
+   apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id"
+});
+// eslint-disable-next-line no-undef
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 export const db = firebaseApp.firestore()
